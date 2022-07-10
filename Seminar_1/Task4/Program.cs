fb7e81414d8c8535b2b882bc3ac636f403f9fbd6
Nikita <nikvs@mail.ru>
2022-07-10 20:38:33 +0700

    добавил вариант решения в Task6

--- Seminar_1/Task4/Program.cs
+++ Seminar_1/Task4/Program.cs
@@ -14,24 +14,33 @@ string? inputThird = Console.ReadLine();
 
 if (inputFirst != null && inputSecond != null && inputThird != null)
 {
-// лобовое решение
-    int inputNumberOne = int.Parse(inputFirst);
-    int inputNumberTwo = int.Parse(inputSecond);
-    int inputNumberThree = int.Parse(inputThird);
-    int max = inputNumberOne;
-    if (inputNumberTwo > max) max = inputNumberTwo;
-    if (inputNumberThree > max) max = inputNumberThree;
-    Console.WriteLine("По классике: " + max);
+    try
+    {
+        // лобовое решение
+        int inputNumberOne = int.Parse(inputFirst);
+        int inputNumberTwo = int.Parse(inputSecond);
+        int inputNumberThree = int.Parse(inputThird);
+        int max = inputNumberOne;
+        if (inputNumberTwo > max) max = inputNumberTwo;
+        if (inputNumberThree > max) max = inputNumberThree;
+        Console.WriteLine("По классике: " + max);
 
-// через Math.Max (в классе Max много методов, в т.ч. и Min)
-    max = Math.Max(inputNumberOne, inputNumberTwo);
-    max = Math.Max(max, inputNumberThree);
-    Console.WriteLine("Через метод Math.Max: " + max);
+        // через Math.Max (в классе Max много методов, в т.ч. и Min)
+        max = Math.Max(inputNumberOne, inputNumberTwo);
+        max = Math.Max(max, inputNumberThree);
+        Console.WriteLine("Через метод Math.Max: " + max);
 
-// через тернарный оператор. сначала проверяется условие (итог true/false), после знака ?
-// первым идёт значение при true, вторым при false 
-    max = inputNumberOne > inputNumberTwo ? inputNumberOne : inputNumberTwo;
-    max = max > inputNumberThree ? max : inputNumberThree;
-    Console.WriteLine("Через тернарный оператор: " + max);
-    
+        // через тернарный оператор. сначала проверяется условие (итог true/false), после знака ?
+        // первым идёт значение при true, вторым при false 
+        max = inputNumberOne > inputNumberTwo ? inputNumberOne : inputNumberTwo;
+        max = max > inputNumberThree ? max : inputNumberThree;
+        Console.WriteLine("Через тернарный оператор: " + max);
+    }
+    catch
+    {
+        // вывод сообщения об ошибке
+        Console.WriteLine("Не корректный ввод!!!");
+
+    }
+
 }
--- Seminar_1/Task6/Program.cs
+++ Seminar_1/Task6/Program.cs
@@ -7,9 +7,27 @@ string? inputLine = Console.ReadLine();
 
 if (inputLine != null)
 {
-    int inputNumber = int.Parse(inputLine);
-    if (inputNumber == 0) Console.Write("Это ноль!!!");
-    else if (inputNumber % 2 == 0) Console.Write("Да, число " + inputNumber + " чётное");
-    else Console.Write("Нет, число " + inputNumber + " нечётное");
+    try
+    {
+        int inputNumber = int.Parse(inputLine);
 
+        // вариант через остаток от деления на 2 
+        Console.WriteLine("Вариант через остаток от деления на 2:");
+        if (inputNumber == 0) Console.WriteLine("Это ноль!!!");
+        else if (inputNumber % 2 == 0) Console.WriteLine("Да, число " + inputNumber + " чётное");
+        else Console.WriteLine("Нет, число " + inputNumber + " нечётное");
+
+        // вариант с проверкой последней цифры (0, 2, 4, 6, 8)
+        Console.WriteLine("Вариант с проверкой последней цифры (0, 2, 4, 6, 8):");
+        string lastNum = inputLine.Substring(inputLine.Length - 1, 1);
+        byte lastNumber = byte.Parse(lastNum);
+        if ((lastNumber == 0) || (lastNumber == 2) || (lastNumber == 4) || (lastNumber == 6) || (lastNumber == 8))
+            Console.WriteLine("Да, число " + inputNumber + " чётное");
+        else Console.WriteLine("Нет, число " + inputNumber + " нечётное");
+    }
+    catch
+    {
+        // вывод сообщения об ошибке
+        Console.WriteLine("Не корректный ввод!!!");
+    }
 }
