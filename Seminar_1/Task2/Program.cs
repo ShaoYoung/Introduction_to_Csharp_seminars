452311e05a70ac1c364568b6416b5b8668b1013a
Nikita <nikvs@mail.ru>
2022-07-10 19:48:33 +0700

    Обернул Task2 в Try Catch

--- Seminar_1/Task2/Program.cs
+++ Seminar_1/Task2/Program.cs
@@ -7,20 +7,46 @@ string? inputFirst = Console.ReadLine();
 Console.Write("Введите второе число: ");
 string? inputSecond = Console.ReadLine();
 
-if (inputFirst != null && inputSecond != null)
+if ((inputFirst != null) && (inputSecond != null))
 {
-    int inputNumberOne = int.Parse(inputFirst);
-    int inputNumberTwo = int.Parse(inputSecond);
-    if (inputNumberOne > inputNumberTwo)
+    // блок try содержит защищенный код, который может вызвать исключение
+    try
     {
-        Console.Write("Первое число большее. max = " + inputNumberOne);
+        // генерация нового исключения при помощи throw
+        // if (inputFirst == "1")
+        // {
+        //     throw new Exception("Ты ввёл цифру 1");
+        // }
+        // парсим первое число
+        int inputNumberOne = int.Parse(inputFirst);
+        // парсим второе число
+        int inputNumberTwo = int.Parse(inputSecond);
+        // сравниваем два числа
+        if (inputNumberOne > inputNumberTwo)
+        {
+            Console.Write("Первое число большее. max = " + inputNumberOne);
+        }
+        else if (inputNumberOne < inputNumberTwo)
+        {
+            Console.Write("Второе число большее. max = " + inputNumberTwo);
+        }
+        else
+        {
+            Console.Write("Числа равны");
+        }
     }
-    else if(inputNumberOne < inputNumberTwo)
+    // catch - обратотка исключений. может использоваться без
+    // аргументов для перехвата любого типа исключения. лучше использовать с аргументами для перехвата
+    // конкретных исключений.
+
+    // catch (Exception e)
+    // {
+    //     Console.WriteLine($"Ошибка: {e.Message}");
+    // }
+
+    catch
     {
-        Console.Write("Второе число большее. max = " + inputNumberTwo);
-    }
-    else
-    {
-        Console.Write("Числа равны");
+        // вывод сообщения об ошибке
+        Console.WriteLine("Не корректный ввод!!!");
     }
 }
