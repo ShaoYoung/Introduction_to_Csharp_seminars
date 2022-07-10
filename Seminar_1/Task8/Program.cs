9681dc5e9eaa51ba44d883d32c00562ecefdea6d
Nikita <nikvs@mail.ru>
2022-07-10 21:02:32 +0700

    в Task8 добавил вариант с коллекцией и строкой

--- Seminar_1/Task8/Program.cs
+++ Seminar_1/Task8/Program.cs
@@ -6,17 +6,47 @@ Console.Write("Введите число: ");
 string? inputLine = Console.ReadLine();
 if (inputLine != null)
 {
-    int inputNumber = int.Parse(inputLine);
-
-    string lineOutput = "";
-    int honest = 2;
-    while (honest <= inputNumber)
+    try
     {
-        lineOutput = lineOutput + honest + ", ";
-        honest += 2;
-    }
-    // убираем лишнюю запятую и пробел в конце итоговой строки
-    string lineOutputClear = lineOutput.Remove(lineOutput.Length - 2, 2);
+        int inputNumber = int.Parse(inputLine);
 
-    Console.WriteLine(lineOutputClear);
+        // вариант через простой вывод чисел, кратных 2
+        string lineOutput = "";
+        int honest = 2;
+        Console.WriteLine("Вариант через простой вывод чисел, кратных 2:");
+        while (honest <= inputNumber)
+        {
+            lineOutput = lineOutput + honest + ", ";
+            honest += 2;
+        }
+        // убираем лишнюю запятую и пробел в конце итоговой строки
+        string lineOutputClear = lineOutput.Remove(lineOutput.Length - 2, 2);
+
+        Console.WriteLine(lineOutputClear);
+
+        // вариант через накопление в массиве и последующий сбор строки
+        lineOutput = "";
+        honest = 2;
+        Console.WriteLine("Вариант через накопление в массиве и последующий сбор строки:");
+        // массив неизвестной длины -->>> коллекция
+        List<int> evenNumbersArray = new List<int>();
+
+        // копим все значения в коллекции evenNumbersArray
+        while (honest <= inputNumber)
+        {
+            evenNumbersArray.Add(honest);
+            honest += 2;
+        }
+
+        // String.Join сцепляет элементы указанного массива или элементы коллекции,
+        // помещая между ними заданный разделитель.
+        lineOutput = String.Join(", ", evenNumbersArray);
+        Console.WriteLine(lineOutput);
+
+    }
+    catch
+    {
+        // вывод сообщения об ошибке
+        Console.WriteLine("Не корректный ввод!!!");
+    }
 }
